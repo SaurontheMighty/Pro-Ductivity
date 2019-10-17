274fad069971f15aa083df98e0374b9315ab527a
Ashish <yesashishs@gmail.com>
2019-10-16 22:30:41 -0400

    Update Manifest

--- popup/pro.js
+++ popup/pro.js
@@ -2,11 +2,8 @@ var x = document.getElementById("textBox").value;
 function startTimer(){
     var start=Date.now();
     var elapsed = (Date.now() - start)/60000;//time elapsed in minutes
-    var y = x;
-    if(elapsed>=x){
-    	y= x-elapsed;
-    	document.getElementById("timeLeft").innerHTML = y;
-    }
+    var y = x-elapsed;
+    document.getElementById("timeLeft").innerHTML = y;
 }
 function stopTimer(){
 
