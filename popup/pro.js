66ae4ac60a74266f494053ef261a6dc62b39065d
Ashish <yesashishs@gmail.com>
2019-10-16 22:32:11 -0400

    Update Timer

--- popup/pro.js
+++ popup/pro.js
@@ -3,7 +3,12 @@ function startTimer(){
     var start=Date.now();
     var elapsed = (Date.now() - start)/60000;//time elapsed in minutes
     var y = x-elapsed;
-    document.getElementById("timeLeft").innerHTML = y;
+    if(y>=0){
+    	document.getElementById("timeLeft").innerHTML = y;
+    }
+    else{
+        document.getElementById("timeLeft").innerHTML = "TIMES UP";
+    }
 }
 function stopTimer(){
 
