c4e1f4c34b11cbaa2d24b1f4e4c19109ed3d209c
Ashish <yesashishs@gmail.com>
2019-10-16 21:52:51 -0400

    Countdown

--- popup/pro.js
+++ popup/pro.js
@@ -1,7 +1,11 @@
 var x = document.getElementById("myNumber").value;
 function startTimer(){
+    var nowDate=new Date();
+    var now=(nowDate.getTime());
+    var end=(nowDate.getTime())+x*60000;
+    var y = end-now;
     document.getElementById("timeLeft").innerHTML = y;
 }
 function stopTimer(){
-    
+
 }
