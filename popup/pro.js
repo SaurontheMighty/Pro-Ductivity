52314d6b7e0e0ac0340a140604f3062105cc478a
Ashish <yesashishs@gmail.com>
2019-10-19 18:25:46 -0400

    Replace inline script

--- popup/pro.js
+++ popup/pro.js
@@ -1,15 +1,24 @@
-var x = document.getElementById("textBox").value;
-function startTimer(){
-    var start=Date.now();
+function listenForClicks() {
+  document.addEventListener("click", (e) => {
+  function ductivity(){
+    var start=Date.now();  
+    var interval = setInterval(function() {
+    var x = document.getElementById("textBox").value;
     var elapsed = (Date.now() - start)/60000;//time elapsed in minutes
-    var y = x-elapsed;
+    var y = Math.round(x - elapsed)
     if(y>=0){
-    	document.getElementById("timeLeft").innerHTML = y;
+      document.getElementById("timeLeft").innerHTML = y;
     }
     else{
         document.getElementById("timeLeft").innerHTML = "TIMES UP";
     }
+  },1000);
+  }
+  function reportError(error) {
+    console.error(`Cannot perform actions on Priveleged Pages: ${error}`);
+  }
+})
 }
-function stopTimer(){
-
-}
+browser.tabs.executeScript({file: "/content_scripts/ductivity.js"})
+.then(listenForClicks)
+.catch(reportExecuteScriptError);
