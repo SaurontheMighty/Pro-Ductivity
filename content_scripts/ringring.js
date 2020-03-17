4bdbb703f52126935d7b7098da3ef2ed02ff79ea
Ashish <yesashishs@gmail.com>
2020-03-17 14:44:09 -0400

    Add timing Functionality

---
+++ content_scripts/ringring.js
@@ -0,0 +1,35 @@
+(function() {
+    /**
+     * Check and set a global guard variable.
+     * If this content script is injected into the same page again,
+     * it will do nothing next time.
+     */
+    if (window.hasRun) {
+      return;
+    }
+    window.hasRun = true;
+
+    /**
+     * Given a URL to a beast image, remove all existing beasts, then
+     * create and style an IMG node pointing to
+     * that image, then insert the node into the document.
+     */
+    function destroy() {
+      document.body.style.border = "5px solid red";
+      document.body.style.MozTransform="rotate(20deg)";
+    }
+
+  
+    /**
+     * Listen for messages from the background script.
+     * Call "beastify()" or "reset()".
+    */
+    browser.runtime.onMessage.addListener((message) => {
+      if (message.command === "ring") {
+        destroy();
+      }
+    });
+  
+  })();
+  
+  
--- popup/pro.js
+++ popup/pro.js
@@ -1,33 +1,47 @@
-var timeSet = document.querySelector("#timerValue");
-var timeLeft = document.querySelector("#timeLeft");
-
 function listenForClicks() {
   document.addEventListener("click", (e) => {
 
-    function time(tabs, timeSet) {
-      browser.tabs.sendMessage(tabs[0].id, {
-        command: "timer",
-        time: timeSet
+    function timer(tabs) {
+      browser.notifications.create({
+        "type": "basic",
+        "title": "Alarm created",
+        "message": `Alarm created`
+      });
+      browser.alarms.clearAll();
+      browser.alarms.create("", {delayInMinutes: 1200});
+      browser.alarms.onAlarm.addListener((alarm) => {
+        var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+        gettingActiveTab.then((tabs) => {
+          browser.tabs.sendMessage(tabs[0].id, {
+            command: "ring"
+          })
         });
+      });
     }
 
+    
+    /**
+     * Just log the error to the console.
+     */
     function reportError(error) {
-      console.error(`Cannot perform actions on Priveleged Pages: ${error}`);
+      console.error(`Could not beastify: ${error}`);
     }
 
-    if (e.target.classList.contains("set")) {
+    if (e.target.classList.contains("timer")) {
       browser.tabs.query({active: true, currentWindow: true})
-        .then(time)
+        .then(timer)
         .catch(reportError);
     }
-  })
+  });
 }
+
+
 function reportExecuteScriptError(error) {
   document.querySelector("#popup-content").classList.add("hidden");
   document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute content script: ${error.message}`);
+  console.error(`Failed to execute beastify content script: ${error.message}`);
 }
 
-browser.tabs.executeScript({file: "/productivity.js"})
+browser.tabs.executeScript({file: "/content_scripts/ringring.js"})
 .then(listenForClicks)
 .catch(reportExecuteScriptError);
