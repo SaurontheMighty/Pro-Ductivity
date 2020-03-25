f2fecc1daa27c168e01e125d9b220946c91a2cd1
Ashish <yesashishs@gmail.com>
2020-03-25 18:36:08 -0400

    Check for correct input

--- options/options.js
+++ options/options.js
@@ -2,29 +2,34 @@ function listenForClicks() {
   document.addEventListener("click", (e) => {
 
     function storeTilt() {
-      browser.notifications.create({
-        "type": "basic",
-        "title": "Saved Settings!",
-        "message": `Tilt: ${(document.querySelector("#tiltBox").value)}`
-      });
-      browser.storage.local.set({tilt: document.querySelector("#tiltBox").value});
+      a=document.querySelector("#tiltBox").value;
+      if(-360<=parseInt(a,10) && parseInt(a,10)<=360){
+        browser.storage.local.set({tilt: document.querySelector("#tiltBox").value});
+        const gettingStoredSettings = browser.storage.local.get();
+        gettingStoredSettings.then(hhh);
+        function hhh(settings){
+          browser.notifications.create({
+            "type": "basic",
+            "title": "Saved Settings!",
+            "message": `Border: ${(settings.tilt)}`
+          });
+        }
+      }
     }
 
     function storeBorder() {
-      browser.notifications.create({
-        "type": "basic",
-        "title": "Saved Settings!",
-        "message": `Border: ${(document.querySelector("#borderBox").value)}`
-      });
-      browser.storage.local.set({border: document.querySelector("#borderBox").value});
-      const gettingStoredSettings = browser.storage.local.get();
-      gettingStoredSettings.then(hhh);
-      function hhh(settings){
-        browser.notifications.create({
-          "type": "basic",
-          "title": "Saved Settings!",
-          "message": `Border: ${(settings.border)}`
-        });
+      a=document.querySelector("#borderBox").value;
+      if(0<=parseInt(a,10) && parseInt(a,10)<=500){
+        browser.storage.local.set({border: document.querySelector("#borderBox").value});
+        const gettingStoredSettings = browser.storage.local.get();
+        gettingStoredSettings.then(hhh);
+        function hhh(settings){
+          browser.notifications.create({
+            "type": "basic",
+            "title": "Saved Settings!",
+            "message": `Border: ${(settings.border)}`
+          });
+        }
       }
     }
 
