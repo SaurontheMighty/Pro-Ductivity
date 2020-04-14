9c7a9c80a877ae76e273b050e93973121bf115b7
Ashish <yesashishs@gmail.com>
2020-04-13 20:55:17 -0400

    Disable polymer is now available from the options menu

--- Chrome/dp.js
+++ Chrome/dp.js
@@ -1,25 +1,22 @@
-
-const pattern1 = "https://www.youtube.com/*";
-
-const filter = {
-    urls: [pattern1]
-}
 var x1="";
 var x2="";
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    chrome.storage.local.get('polymer', function (settings) {
-        console.log('22');
-        if(settings.polymer===false){
-            if(!changeInfo.url){
-                return;
+    if(tab.url.includes("youtube.com")){
+        chrome.storage.local.get('polymer', function (settings) {
+            console.log('22');
+            if(settings.polymer===false){
+                x=tab.url;
+                if(x!=x1 && x.includes("v") && !x.includes("disable_polymer") && x!=x2){
+                    chrome.tabs.update(null, {url: x+"&disable_polymer=true"});
+                    x1=x+"&disable_polymer=true";
+                    x2=x;
+                }else if(x!=x1 && x!=x2 && !x.includes("v")){
+                    chrome.tabs.update(null, {url: x+"?disable_polymer=true"});
+                    x1=x+"?disable_polymer=true";
+                    x2=x;
+                }
             }
-            x=tab.url;
-            if(x!=x1 && x.includes("watch") && !x.includes("disable_polymer") && x!=x2){
-                chrome.tabs.update(null, {url: x+"&disable_polymer=true"});
-                x1=x+"&disable_polymer=true";
-                x2=x;
-            }
-        }
-    });
-},filter);     
+        });
+    }
+}); 
